cdbb227a614e9f0fb028b899337aed68c355ef97
JDWoodcock <jw745@cam.ac.uk>
2017-02-07 10:20:30 +0000

    Add files via upload

--- Poisson model stats19.R
+++ Poisson model stats19.R
@@ -1,5 +1,3 @@
-#installing packages
-#install.packages("readstata13")
 #install.packages("tidyr")
 #install.packages("dplyr")
 
@@ -8,8 +6,10 @@ library(dplyr)
 library(tidyr)
 
 #reading in the data
-load("Stats19.rda")
+ss19<-read.dta13("C:/Users/jw745/Dropbox/z_ITHIMfiles/Stats19/1b_DataCreated/Stats19_05-15_ready_v3.dta")
 ss19<-tbl_df(ss19)
+ss19<-filter(ss19, strike_mode!='No other vehicle')
+ss19<-ss19[complete.cases(ss19),]
 #adding age bands
 ss19$cas_age_band= cut(ss19$cas_age, breaks = c(0, 20,40,60,80,110), right = FALSE)
 ss19$strike_age_band= cut(ss19$strike_age, breaks = c(0, 16,25,60,80,110), right = FALSE)
@@ -22,41 +22,24 @@ library(tidyverse)
 # [4] "cas_severity"   "cas_mode"       "cas_male"      
 # [7] "strike_mode"    "strike_male"   "strike_age_band"
 ###
-ss19 <- select(ss19, everything()) %>%
-  filter(cas_male %in% c(1, 0)) %>%
-  filter(cas_mode %in% c("cyclist","pedestrian", "car/taxi"))
-
 ssg <- group_by(ss19, cas_mode, cas_male, cas_severity, strike_mode, cas_age_band, strike_age_band, roadtype, strike_male) %>% 
-#  select (ss19, cas_mode, cas_male, cas_severity, strike_mode, cas_age_band, strike_age_band, roadtype, strike_male) %>% 
+  #  select (ss19, cas_mode, cas_male, cas_severity, strike_mode, cas_age_band, strike_age_band, roadtype, strike_male) %>% 
   summarise(count=n()) %>% 
   droplevels() %>% 
   as.data.frame() %>%    # remove "grouped" class, which breaks filling with zeroes
   complete(cas_mode, cas_male, cas_severity, strike_mode, strike_age_band, cas_age_band, strike_male, roadtype, fill=list(count=0))
 
-##this is the model I want to fit
-fit <-
-  glm(count ~  strike_mode* roadtype* (cas_mode *cas_severity* cas_age_band* cas_male* strike_age_band  + strike_age_band*strike_male), 
+#new version modeled for each road type- new specification with nov dropped
+ssg<-filter(ssg,roadtype=="A")
+fit<-
+  glm(count ~(cas_age_band*cas_severity + cas_male*cas_severity*cas_age_band + strike_male*strike_age_band + strike_mode*cas_severity*strike_age_band*cas_male*strike_male+cas_mode*cas_severity*cas_age_band*strike_age_band*cas_male*strike_mode),
       data=ssg, family=poisson)
 
-##this is the next best model- needs to be done for each strike_mode and road_type
-fit <-
-  glm(count ~  (cas_mode *cas_severity* cas_age_band* cas_male* strike_age_band  + strike_age_band*strike_male), 
-      data=ssg, family=poisson)
 
-#next best model for cars on a roads
-ssg<-filter(ssg,roadtype=='A' & strike_mode=='car/taxi')
-fit <-
-  glm(count ~  (cas_mode *cas_severity* cas_age_band* cas_male* strike_age_band  + strike_age_band*strike_male), 
-      data=ssg, family=poisson)
-#this is the minimum model needed to run the total model from - if done separately for strike_mode* roadtype* strike_age_band-#
-#although strike_age_band is is more complicated
-fit <-
-  glm(count ~  (cas_mode *cas_severity* cas_age_band* cas_male* strike_age_band), 
-      data=ssg, family=poisson)
 #this is Excel ithim 1 structure
-fit_ITHIM1 <-
-  glm(count ~  (cas_mode *cas_severity* roadtype* strike_mode), 
-      data=ssg, family=poisson)
+#fit_ITHIM1 <-
+#  glm(count ~  (cas_mode *cas_severity* roadtype* strike_mode), 
+#     data=ssg, family=poisson)
 ## add predicted count with standard error 
 pred <- predict(fit, se.fit=TRUE, type="response")
 ssg <- mutate(ssg, count_fit=pred[["fit"]], count_se=pred[["se.fit"]])
