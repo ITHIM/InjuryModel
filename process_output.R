2344d603643ce1a5d89a097b57b3b0ad683feb4d
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-09 10:57:25 +0000

    to process ouptut from middle.R

--- process_output.R
+++ process_output.R
@@ -1,13 +1,13 @@
 
-#JW code to transform output
+#JW code to transform output into the ITHIM model
 
 library(tidyverse)
-output<-read.dta13("C:/Users/jw745/Dropbox/z_ITHIMfiles/Stats19/1b_DataCreated/Stats19_05-15_ready_v3.dta")
-output <- group_by(output, cas_mode, cas_severity, strike_mode, roadtype) %>%
+stats19_0515 <- readRDS("stats19_05-15_ready_v3.Rds")
+output <- group_by(stats19_0515, cas_mode, cas_severity, strike_mode, roadtype) %>%
   summarise(count=n()) %>%
   droplevels() %>%
   as.data.frame() %>%    # remove "grouped" class, which breaks filling with zeroes
   complete(cas_mode, cas_severity, strike_mode, roadtype, fill=list(count=0)) %>%
   spread (cas_mode, count)%>%
   arrange(cas_severity, roadtype, strike_mode)
-select(output, one_of(c("cas_severity", "roadtype", "strike_mode","pedestrian", "cyclist", "bus", 'car/taxi', "heavy goods", "light goods", "motorcycle", "No other vehicle")))
+select(output, one_of(c("cas_severity", "roadtype", "strike_mode","pedestrian", "cyclist", "bus", 'car/taxi', "heavy goods", "light goods", "motorcycle", "other or unknown")))
