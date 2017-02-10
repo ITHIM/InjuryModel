dfa2447b30b068591ea89363bc4336f4a771ed94
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-10 11:56:18 +0000

    the little example is no longer needed

--- test.R
+++
@@ -1,6 +0,0 @@
-test <- read.table("test.dat",header=TRUE)
-
-library(dplyr)
-by_test <- test %>% group_by(x,y,z)
-ntest <- mutate(arrange(test,x,y,z), i=unlist(lapply(group_size(by_test),FUN=seq_len)))
-ntest
